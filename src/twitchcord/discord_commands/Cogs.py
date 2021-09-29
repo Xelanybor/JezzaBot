3013ab8c71aa8da7de1f4edc6e734e495881c06d
Excel <excelthebeanman@gmail.com>
2021-09-29 17:13:22 +0200

    runtime cogs
    Added commands for loading, unloading, and reloading cogs at runtime.

--- src/twitchcord/discord_commands/Cogs.py
+++ src/twitchcord/discord_commands/Cogs.py
@@ -1,4 +1,42 @@
 from discord.ext import commands
+from discord.ext.commands.errors import ExtensionAlreadyLoaded, ExtensionFailed, ExtensionNotFound, ExtensionNotLoaded, NoEntryPointError
 
 class Cogs(commands.Cog):
-    "Loads, unloads and reloads command cogs."
+    "Loads, unloads, and reloads command cogs."
+
+    def __init__(self, client: commands.Bot):
+        self.client = client
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def load(self, ctx: commands.Context, cog: str):
+        "Load a given cog."
+        try:
+            self.client.load_extension(f"twitchcord.discord_commands.{cog}")
+            await ctx.send(f"Successfully loaded cog `{cog}`.")
+        except ExtensionNotFound as e:
+            await ctx.send(f"Cog `{e.name}` was unable to be found!")
+        except ExtensionAlreadyLoaded as e:
+            await ctx.send(f"Cog `{e.name}` is already loaded!")
+        except NoEntryPointError or ExtensionFailed as e:
+            await ctx.send(f"Unable to load cog: {e}")
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def unload(self, ctx: commands.Context, cog: str):
+        "Unload a given cog."
+        try:
+            self.client.unload_extension(f"twitchcord.discord_commands.{cog}")
+        except ExtensionNotFound as e:
+            await ctx.send(f"Cog `{e.name}` was unable to be found!")
+        except ExtensionNotLoaded as e:
+            await ctx.send(f"Cog `{e.name}` is already unloaded!")
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def reload(self, ctx: commands.Context, cog: str):
+        "Reload a given cog."
+        try:
+            self.client.reload_extension(f"twitchcord.discord_commands.{cog}")
+        except ExtensionNotFound as e:
+            await ctx.send(f"Cog `{e.name}` was unable to be found!")

1121afc0b9e13baa00fd586292bb0f37aea04eb2
Excel <excelthebeanman@gmail.com>
2021-10-04 23:25:13 +0200

    Updated cogs perms
    Cogs can now only be manipulated at runtime by me. Yes this power will go immediately to my head. No I do not care.

--- src/twitchcord/discord_commands/Cogs.py
+++ src/twitchcord/discord_commands/Cogs.py
@@ -1,5 +1,24 @@
+import discord
 from discord.ext import commands
 from discord.ext.commands.errors import ExtensionAlreadyLoaded, ExtensionFailed, ExtensionNotFound, ExtensionNotLoaded, NoEntryPointError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DISCORD_DEV_ID = os.getenv("DISCORD_DEV_ID")
+
+class NotDevError(commands.CheckFailure):
+    """Error that is raised when someone who isn't a dev tries to use a dev-only command."""
+    pass
+
+def isDev():
+    """Check to lock a command for only devs."""
+    async def wrapper(ctx):
+            if ctx.author.id == int(DISCORD_DEV_ID):
+                return True
+            else:
+                raise NotDevError("You don't have permission to use this command.")
+    return commands.check(wrapper)
 
 class Cogs(commands.Cog):
     "Loads, unloads, and reloads command cogs."
@@ -7,39 +26,55 @@ class Cogs(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
-    @commands.command()
-    @commands.has_permissions(administrator=True)
+    @commands.command(hidden=True)
+    @isDev()
     async def load(self, ctx: commands.Context, cog: str):
         "Load a given cog."
         try:
             self.client.load_extension(f"twitchcord.discord_commands.{cog}")
             await ctx.send(f"Successfully loaded cog `{cog}`.")
-        except ExtensionNotFound as e:
-            await ctx.send(f"Cog `{e.name}` was unable to be found!")
-        except ExtensionAlreadyLoaded as e:
-            await ctx.send(f"Cog `{e.name}` is already loaded!")
+        except ExtensionNotFound:
+            await ctx.send(f"Cog `{cog}` was unable to be found!")
+        except ExtensionAlreadyLoaded:
+            await ctx.send(f"Cog `{cog}` is already loaded!")
         except NoEntryPointError or ExtensionFailed as e:
             await ctx.send(f"Unable to load cog: {e}")
 
-    @commands.command()
-    @commands.has_permissions(administrator=True)
+    @commands.command(hidden=True)
+    @isDev()
     async def unload(self, ctx: commands.Context, cog: str):
         "Unload a given cog."
         try:
             self.client.unload_extension(f"twitchcord.discord_commands.{cog}")
-        except ExtensionNotFound as e:
-            await ctx.send(f"Cog `{e.name}` was unable to be found!")
-        except ExtensionNotLoaded as e:
-            await ctx.send(f"Cog `{e.name}` is already unloaded!")
+            await ctx.send(f"Successfully unloaded cog `{cog}`.")
+        except ExtensionNotFound:
+            await ctx.send(f"Cog `{cog}` was unable to be found!")
+        except ExtensionNotLoaded:
+            await ctx.send(f"Cog `{cog}` is already unloaded!")
 
-    @commands.command()
-    @commands.has_permissions(administrator=True)
+    @commands.command(hidden=True)
+    @isDev()
     async def reload(self, ctx: commands.Context, cog: str):
         "Reload a given cog."
         try:
             self.client.reload_extension(f"twitchcord.discord_commands.{cog}")
-        except ExtensionNotFound as e:
-            await ctx.send(f"Cog `{e.name}` was unable to be found!")
+            await ctx.send(f"Successfully reloaded cog `{cog}`")
+        except ExtensionNotFound:
+            await ctx.send(f"Cog `{cog}` was unable to be found!")
+        except ExtensionNotLoaded:
+            await ctx.send(f"Cog `{cog}` must first be loaded!")
+
+    # Ignore dev errors
+    @load.error
+    @unload.error
+    @reload.error
+    async def dev_error(self, ctx, error):
+        if isinstance(error, NotDevError):
+            await ctx.send(f"`{error}`")
+        else:
+            print(error)
+
+    
 
 def setup(client: commands.Bot):
     client.add_cog(Cogs(client))
